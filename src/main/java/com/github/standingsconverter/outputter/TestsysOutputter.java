c36537713e790670842b61694cec51a140cd6f66
Alexey Dergunov <alexey.dergunov@gmail.com>
2015-10-22 01:15:31 +0400

    Added EjudgeParser and TestsysOutputter

---
+++ src/main/java/com/github/standingsconverter/outputter/TestsysOutputter.java
@@ -0,0 +1,69 @@
+package com.github.standingsconverter.outputter;
+
+import com.github.standingsconverter.entity.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class TestsysOutputter implements Outputter {
+    @Override
+    public void output(Contest contest, String outputFile) throws IOException {
+        List<Problem> problems = contest.getProblems();
+        List<Team> teams = contest.getTeams();
+        List<Submission> submissions = contest.getSubmissions();
+        try (PrintWriter writer = new PrintWriter(new File(outputFile), "UTF-8")) {
+            writer.println((char) 0x1A);
+            writer.printf("@contest \"%s\"", contest.getName());
+            writer.println();
+            writer.printf("@contlen %d", contest.getDuration());
+            writer.println();
+            writer.printf("@problems %d", problems.size());
+            writer.println();
+            writer.printf("@teams %d", teams.size());
+            writer.println();
+            writer.printf("@submissions %d", submissions.size());
+            writer.println();
+            for (Problem problem : problems) {
+                writer.println(toString(problem));
+            }
+            for (Team team : teams) {
+                writer.println(toString(team));
+            }
+            for (Submission submission : submissions) {
+                writer.println(toString(submission));
+            }
+            writer.close();
+        }
+    }
+
+    private static String toString(Problem problem) {
+        return String.format("@p %c,%s,20,0", problem.getId(), problem.getName());
+    }
+
+    private static String toString(Team team) {
+        return String.format("@t %d,0,1,\"%s\"", team.getId(), team.getName());
+    }
+
+    private static String toString(Submission submission) {
+        return String.format("@s %d,%c,%d,%d,%s", submission.getTeam().getId(), submission.getProblem().getId(), submission.getAttempt(), submission.getTime(), toString(submission.getVerdict()));
+    }
+
+    private static String toString(Verdict verdict) {
+        // TODO seems that CF doesn't support IL and SV, they are replaced with TL and RT
+        switch (verdict) {
+            case ACCEPTED: return "OK";
+            case REJECTED: return "RJ";
+            case WRONG_ANSWER: return "WA";
+            case RUNTIME_ERROR: return "RT";
+            case TIME_LIMIT_EXCEEDED: return "TL";
+            case MEMORY_LIMIT_EXCEEDED: return "ML";
+            case COMPILATION_ERROR: return "CE";
+            case PRESENTATION_ERROR: return "PE";
+            case IDLENESS_LIMIT_EXCEEDED: return "TL";
+            case SECURITY_VIOLATION: return "RT";
+        }
+        throw new IllegalArgumentException("Unknown verdict: " + verdict);
+    }
+}
---
+++ src/main/java/com/github/standingsconverter/parser/EjudgeParser.java
@@ -0,0 +1,142 @@
+package com.github.standingsconverter.parser;
+
+import com.github.standingsconverter.entity.*;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class EjudgeParser implements Parser {
+    @Override
+    public Contest parse(String inputFile) throws IOException {
+        DocumentBuilder builder;
+        Document document;
+        try {
+            builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            document = builder.parse(new File(inputFile));
+        } catch (ParserConfigurationException | SAXException e) {
+            throw new IOException(e);
+        }
+        Element runLogElement = document.getDocumentElement();
+        long duration = Long.parseLong(runLogElement.getAttribute("duration")) / 60;
+        String name = runLogElement.getElementsByTagName("name").item(0).getTextContent();
+        Map<Integer, Team> teams = parseTeams(runLogElement.getElementsByTagName("user"));
+        Map<Integer, Problem> problems = parseProblems(runLogElement.getElementsByTagName("problem"));
+        Map<Integer, Submission> submissions = parseSubmissions(teams, problems, runLogElement.getElementsByTagName("run"));
+        List<Team> teamList = new ArrayList<>(teams.values());
+        List<Problem> problemList = new ArrayList<>(problems.values());
+        List<Submission> submissionList = new ArrayList<>(submissions.values());
+        Collections.sort(teamList, (o1, o2) -> Integer.compare(o1.getId(), o2.getId()));
+        Collections.sort(problemList, (o1, o2) -> Integer.compare(o1.getId(), o2.getId()));
+        Collections.sort(submissionList, (o1, o2) -> {
+            if (o1.getTime() != o2.getTime()) {
+                return Long.compare(o1.getTime(), o2.getTime());
+            }
+            if (o1.getAttempt() != o2.getAttempt()) {
+                return Integer.compare(o1.getAttempt(), o2.getAttempt());
+            }
+            return Integer.compare(o1.getId(), o2.getId());
+        });
+        return new Contest(name, duration, problemList, teamList, submissionList);
+    }
+
+    private Map<Integer, Team> parseTeams(NodeList nodeList) {
+        Map<Integer, Team> map = new HashMap<>();
+        Map<Integer, Integer> ids = new HashMap<>();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            NamedNodeMap attributes = node.getAttributes();
+            int id = Integer.parseInt(attributes.getNamedItem("id").getNodeValue());
+            String name = attributes.getNamedItem("name").getNodeValue();
+            int newID = ids.size();
+            ids.put(id, newID);
+            Team team = new Team(newID, name);
+            map.put(id, team);
+        }
+        return map;
+    }
+
+    private Map<Integer, Problem> parseProblems(NodeList nodeList) {
+        Map<Integer, Problem> map = new HashMap<>();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            NamedNodeMap attributes = node.getAttributes();
+            int id = Integer.parseInt(attributes.getNamedItem("id").getNodeValue());
+            String name = attributes.getNamedItem("short_name").getNodeValue();
+            Problem problem = new Problem(name.charAt(0), name);
+            map.put(id, problem);
+        }
+        return map;
+    }
+
+    private Map<Integer, Submission> parseSubmissions(Map<Integer, Team> teams, Map<Integer, Problem> problems, NodeList nodeList) {
+        Map<TeamProblemKey, Integer> attempts = new HashMap<>();
+        Map<Integer, Submission> map = new HashMap<>();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            NamedNodeMap attributes = node.getAttributes();
+            int id = Integer.parseInt(attributes.getNamedItem("run_id").getNodeValue());
+            long time = Long.parseLong(attributes.getNamedItem("time").getNodeValue());
+            int teamId = Integer.parseInt(attributes.getNamedItem("user_id").getNodeValue());
+            int problemId = Integer.parseInt(attributes.getNamedItem("prob_id").getNodeValue());
+            String verdict = attributes.getNamedItem("status").getNodeValue();
+            Team team = teams.get(teamId);
+            Problem problem = problems.get(problemId);
+            TeamProblemKey key = new TeamProblemKey(teamId, problemId);
+            if (!attempts.containsKey(key)) {
+                attempts.put(key, 1);
+            } else {
+                attempts.put(key, attempts.get(key) + 1);
+            }
+            int attempt = attempts.get(key);
+            Submission submission = new Submission(id, team, problem, attempt, time, parseVerdict(verdict));
+            map.put(id, submission);
+        }
+        return map;
+    }
+
+    private static Verdict parseVerdict(String s) {
+        switch (s) {
+            case "OK": return Verdict.ACCEPTED;
+            case "CE": return Verdict.COMPILATION_ERROR;
+            case "RT": return Verdict.RUNTIME_ERROR;
+            case "TL": return Verdict.TIME_LIMIT_EXCEEDED;
+            case "PE": return Verdict.PRESENTATION_ERROR;
+            case "WA": return Verdict.WRONG_ANSWER;
+            case "ML": return Verdict.MEMORY_LIMIT_EXCEEDED;
+            case "SE": return Verdict.SECURITY_VIOLATION;
+            case "RJ": return Verdict.REJECTED;
+        }
+        throw new IllegalArgumentException("Unknown verdict: " + s);
+    }
+
+    private static class TeamProblemKey {
+        private final int teamId;
+        private final int problemId;
+
+        public TeamProblemKey(int teamId, int problemId) {
+            this.teamId = teamId;
+            this.problemId = problemId;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            TeamProblemKey that = (TeamProblemKey) o;
+            return problemId == that.problemId && teamId == that.teamId;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = teamId;
+            result = 31 * result + problemId;
+            return result;
+        }
+    }
+}
